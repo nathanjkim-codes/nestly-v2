90352e6d7752d84e0b293916e85d2575c7a968b1
Jung hwan Kim (Nathan) <nathanjungkim.dev@gmail.com>
2026-06-25 22:27:41 -0700

    feat(header): connect selected child state with props

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,36 @@
 import Sidebar from "./components/Sidebar.jsx";
 import Header from "./components/Header.jsx";
 import Dashboard from "./components/Dashboard.jsx";
+import { useState } from "react";
 
 function App() {
+  // Mock Data
+  const children = [
+    {
+      id: 1,
+      name: "Emma",
+      birth: "6-3-2023",
+      height: "22 in",
+      weight: "7.5 lbs",
+      sleep: "14 hrs",
+    },
+
+    {
+      id: 2,
+      name: "Evelyn",
+      birth: "3-24-2026",
+      height: "20in",
+      weight: "7 lbs",
+      sleep: "13 hrs",
+    },
+  ];
+
+  // useState
+  const [selectedChildId, setSelectedChildId] = useState(children[0].id);
+
+  // selectedChild
+  const selectedChild = children.find((child) => child.id === selectedChildId);
+
   return (
     <div className="app-shell">
       <Sidebar />
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,13 +1,20 @@
-function Header() {
+function Header({
+  children,
+  selectedChildId,
+  setSelectedChildId,
+  selectedChild,
+}) {
   return (
     <div className="header">
       <div className="header-info">
         <h1 className="header-title">Good morning, Sarah! 👋</h1>
-        <p className="header-subtitle">Here's how Emma is doing today.</p>
+        <p className="header-subtitle">
+          Here's how {selectedChild.name} is doing today.
+        </p>
       </div>
       <div className="header-actions">
         <button className="add-record-button">+ Add Record</button>
-        <button className="child-selector">Emma ▼</button>
+        <button className="child-selector">{selectedChild.name} ▼</button>
         <button className="notification-button">🔔</button>
       </div>
     </div>
