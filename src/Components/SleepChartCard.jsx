a94ac92eaeda8bc7b912eb1a048efef81cd3cf75
Jung hwan Kim (Nathan) <nathanjungkim.dev@gmail.com>
2026-07-07 14:14:49 -0700

    feat(sleep-chart): add dynamic average sleep calculation

--- src/Components/SleepChartCard.jsx
+++ src/Components/SleepChartCard.jsx
@@ -9,6 +9,27 @@ import {
 } from "recharts";
 
 function SleepChartCard({ sleepRecords }) {
+  const sleepDurationsSum = function (sleepRecords) {
+    let durationSum = 0;
+    for (let i = 0; i < sleepRecords.length; i++) {
+      durationSum += sleepRecords[i].duration;
+    }
+    return durationSum;
+  };
+  const totalSleepDuration = sleepDurationsSum(sleepRecords);
+
+  const averageSleepDuration = totalSleepDuration / sleepRecords.length;
+  console.log(averageSleepDuration);
+
+  const formatDecimalHours = function (averageSleepDuration) {
+    const sleepHours = Math.floor(averageSleepDuration);
+    const sleepMinutes = Math.round((averageSleepDuration - sleepHours) * 60);
+    return `${sleepHours} hr ${sleepMinutes} m`;
+  };
+
+  const formattedAverageSleepDuration =
+    formatDecimalHours(averageSleepDuration);
+
   return (
     <div className="dashboard-card sleep-chart-card">
       <div className="chart-header">
@@ -16,7 +37,7 @@ function SleepChartCard({ sleepRecords }) {
         <button className="card-action-button">View all</button>
       </div>
       <div className="chart-legend">
-        <p className="legend-sleep-info">Avg 9h 20m</p>
+        <p className="legend-sleep-info">Avg {formattedAverageSleepDuration}</p>
       </div>
       <div className="sleep-chart-area">
         <ResponsiveContainer width="100%" height="100%">
