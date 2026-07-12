41753927a500f11eb0de1fd7657e89ca2c4340e1
Jung hwan Kim (Nathan) <nathanjungkim.dev@gmail.com>
2026-07-12 00:27:15 -0700

    feat(recent-record): normalize recent records for display

--- src/Components/RecentRecordsCard.jsx
+++ src/Components/RecentRecordsCard.jsx
@@ -20,6 +20,49 @@ function RecentRecordsCard({ growthRecords, sleepRecords, feedingRecords }) {
     latestFeedingRecord,
   ].filter((record) => record !== null);
 
+  const formattedRecordDate = function (date) {
+    const dateObject = new Date(date);
+    const dateString = dateObject.toLocaleDateString("en-US", {
+      month: "short",
+      day: "numeric",
+    });
+    return `${dateString}`;
+  };
+
+  const formatDecimalHours = function (duration) {
+    const sleepHours = Math.floor(duration);
+    const sleepMinutes = Math.round((duration - sleepHours) * 60);
+    return `${sleepHours} hr ${sleepMinutes} m`;
+  };
+
+  const normalizedRecords = recentRecords.map((record) => {
+    if (Object.hasOwn(record, "height")) {
+      return {
+        type: "growth",
+        title: "Growth",
+        date: formattedRecordDate(record.date),
+        value: `${record.height} in ${record.weight} lbs`,
+        icon: "📏",
+      };
+    } else if (Object.hasOwn(record, "duration")) {
+      return {
+        type: "sleep",
+        title: "Sleep",
+        date: formattedRecordDate(record.date),
+        value: formatDecimalHours(record.duration),
+        icon: "🌙",
+      };
+    } else if (Object.hasOwn(record, "amount")) {
+      return {
+        type: "feeding",
+        title: "Feeding",
+        date: formattedRecordDate(record.date),
+        value: `${record.amount} ${record.unit}`,
+        icon: "🍼",
+      };
+    }
+  });
+
   return (
     <div className="dashboard-card recent-records-card">
       <div className="chart-header">
