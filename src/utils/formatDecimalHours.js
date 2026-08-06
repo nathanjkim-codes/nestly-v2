1a9f9fceda4f82f531852bc3221ab098a3174e44
Jung hwan Kim (Nathan) <nathanjungkim.dev@gmail.com>
2026-08-05 22:35:11 -0700

    refactor(utils): improve duration format

--- src/utils/formatDecimalHours.js
+++ src/utils/formatDecimalHours.js
@@ -3,6 +3,8 @@ export function formatDecimalHours(decimalHours) {
   const sleepMinutes = Math.round((decimalHours - sleepHours) * 60);
   if (sleepHours === 0) {
     return `${sleepMinutes} m`;
+  } else if (sleepMinutes === 0) {
+    return `${sleepHours} h`;
   } else {
     return `${sleepHours} h ${sleepMinutes} m`;
   }
